bc3c4b1d9fee097899c6387afad45c8a9f9d7f1a
Moses-Ongwenyi <moses.ongwenyi@ibm.com>
2020-04-02 03:14:25 +0300

    setting up configuration for different environments.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 
+import config
 
 app = Flask(__name__)
+if app.config.get('ENV') == 'production':
+    app.config.from_object(config.ProductionConfig)
+elif app.config.get('ENV') == 'staging':
+    app.config.from_object(config.StagingConfig)
+elif app.config.get('ENV') == 'development':
+    app.config.from_object(config.DevelopmentConfig)
+else:
+    app.config.from_object(config.TestingConfig)
 
 
 from app import routes  # noqa
--- config.py
+++ config.py
@@ -1,8 +1,40 @@
+import os
+from dotenv import load_dotenv
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
-    pass
+    load_dotenv('env/.env')
+    DEBUG = os.getenv('DEBUG')
+    TESTING = os.getenv('TESTING')
+    CSRF_ENABLED = os.getenv('CSRF_ENABLED')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv(
+        'SQLALCHEMY_TRACK_MODIFICATIONS')
 
-class ProdConfig(Config):
-    pass
 
-class DevConfig(Config):
+class ProductionConfig(Config):
+    load_dotenv('env/prod.env', override=True)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+
+class StagingConfig(Config):
+    load_dotenv('env/staging.env', override=True)
+    TESTING = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+
+class DevelopmentConfig(Config):
+    load_dotenv('env/dev.env', override=True)
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+
+
+class TestingConfig(Config):
+    load_dotenv('env/testing.env', override=True)
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
