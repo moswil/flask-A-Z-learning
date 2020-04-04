c671699cb41e86fb3eb95154222e3d385d7bddc4
Moses-Ongwenyi <moses.ongwenyi@ibm.com>
2020-04-04 13:46:38 +0300

    created tags table and many-to-many relationship.

--- app/models.py
+++ app/models.py
@@ -17,6 +17,12 @@ class User(db.Model):
         self.email = email
 
 
+tags = db.Table('post_tags',
+                db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+                )
+
+
 class Post(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(128), nullable=False)
@@ -28,6 +34,11 @@ class Post(db.Model):
         lazy='dynamics'
     )
     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    tags = db.relationship(
+        'Tag',
+        secondary=tags,
+        backref=db.backref('posts', lazy='dynamic')
+    )
 
     def __init__(self, title, text=None, publish_date=None):
         self.text = text
@@ -46,3 +57,11 @@ class Comment(db.Model):
         self.name = name
         self.text = text
         self.date = date
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, title):
+        self.title = title
